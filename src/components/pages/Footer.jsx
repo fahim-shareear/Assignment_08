1c306d23faf70df0a2f2d23bf3bb6a33ff39fa81
arctic-shark <fahimshareear@gmail.com>
2025-12-21 02:27:56 +0600

    adding responsive footer

--- src/components/pages/Footer.jsx
+++ src/components/pages/Footer.jsx
@@ -4,9 +4,10 @@ import { Codepen, Facebook, Github, Twitter } from 'lucide-react';
 
 const Footer = () => {
     return (
-        <div className="bg-[#001931]  mx-auto rounded-t-xl max-h-75">
-            <div className="m-5 lg:max-w-11/12 mx-auto p-2 flex items-center justify-between">
-                <div className="border-2 border-red-500 max-w-150">
+        <div className="bg-[#001931]  mx-auto rounded-t-xl max-h-auto">
+            <div className="m-5 lg:max-w-11/12 mx-auto p-2 flex flex-col items-start gap-6
+                lg:flex-row lg:items-center lg:justify-between">
+                <div className="max-w-150">
                     <div className="flex items-center ml-1">
                         <img src={Logo} alt="logo" className="w-10 h-10"/>
                         <h1 className="uppercase font-bold text-purple-500">hero.io</h1>
@@ -18,7 +19,7 @@ const Footer = () => {
                     <p className="text-[13px] p-1">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ex dolor soluta, corrupti iusto tempora cumque, dicta natus enim veritatis ad asperiores, quae aliquid cum commodi praesentium dolores neque reprehenderit minus.</p>
                 </div>
                 <div>
-                   <div className="flex items-left flex-col">
+                   <div className="flex flex-col items-start">
                     <h1 className="capitalize font-bold mb-1">social links</h1>
                         <Facebook className="rounded-full bg-[#03315b] hover:bg-[#034888] transition-all duration-400 linear cursor-pointer p-1"></Facebook>
                         <Github className="rounded-full bg-[#03315b]   hover:bg-[#034888] transition-all duration-400 linear cursor-pointer p-1"></Github>
