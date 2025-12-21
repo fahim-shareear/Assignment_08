87ec01b66dda73d4b8351445545660e56ea6fbd6
arctic-shark <fahimshareear@gmail.com>
2025-12-21 23:50:45 +0600

    adding style to download font in all the grid

--- src/components/Griddata/Griddata.jsx
+++ src/components/Griddata/Griddata.jsx
@@ -16,11 +16,11 @@ const Griddata = ({promise}) => {
                     <div className="flex items-center justify-between mt-1">
                         <div className="flex items-center gap-2 bg-gray-200 p-1 rounded-md">
                             <img src={Downloadicon} alt="downloadicon" className="w-4 h-4"/>
-                            <p className="text-green-500 font-bold">{promise.downloads}</p>
+                            <p className="text-green-500 font-bold uppercase">{promise.downloads}</p>
                         </div>
                         <div className="flex items-center gap-2 bg-gray-200 p-1 rounded-md">
                             <img src={Ratingicon} alt="ratingicon" className="w-4 h-4"/>
-                            <p className="text-green-500 font-bold">{promise.ratingAvg}</p>
+                            <p className="text-green-500 font-bold uppercase">{promise.ratingAvg}</p>
                         </div>
                     </div>
                 </div>
--- src/components/pages/menu/Gridforapp/Gridforapppage.jsx
+++ src/components/pages/menu/Gridforapp/Gridforapppage.jsx
@@ -15,11 +15,11 @@ const Gridforapppage = ({appsshow}) => {
                     <div className="flex items-center justify-between mt-1">
                         <div className="flex items-center gap-2 bg-gray-200 p-1 rounded-md">
                             <img src={Downloadicon} alt="downloadicon" className="w-4 h-4"/>
-                            <p className="text-green-500 font-bold">{appsshow.downloads}</p>
+                            <p className="text-green-500 font-bold uppercase">{appsshow.downloads}</p>
                         </div>
                         <div className="flex items-center gap-2 bg-gray-200 p-1 rounded-md">
                             <img src={Ratingicon} alt="ratingicon" className="w-4 h-4"/>
-                            <p className="text-green-500 font-bold">{appsshow.ratingAvg}</p>
+                            <p className="text-green-500 font-bold uppercase">{appsshow.ratingAvg}</p>
                         </div>
                     </div>
                 </div>
