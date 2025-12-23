d5f01ad456e5cc2384abe6cfc97a73fd4fc0d7ca
arctic-shark <fahimshareear@gmail.com>
2025-12-23 10:56:23 +0600

    adding functionality and style to PageNotFound && Navigation page

--- src/components/errorpages/PageNotFound.jsx
+++ src/components/errorpages/PageNotFound.jsx
@@ -1,20 +1,16 @@
-import React, { useState } from 'react';
 import Pagenotfound from '../../assets/error-404.png';
-import { Link } from 'lucide-react';
-import { NavLink } from 'react-router';
+import { NavLink, useNavigate } from 'react-router';
 
 const PageNotFound = () => {
-    const [pagenotfound, setPageNotFound] = useState();
-    const handlePageBack = () =>{
-        setPageNotFound(pagenotfound - 1);
-    };
+    const navigate = useNavigate();
+
     return (
         <div className="w-full h-screen flex items-center justify-center">
             <div className="lg:max-w-11/12 flex items-center flex-col mt-20">
                 <img src={Pagenotfound} alt="error 404" width={400} height={400} className="mb-5"/>
                 <h1 className="font-bold text-4xl mt-4 p-2 text-black">Oops, page not found!</h1>
                 <p className="text-gray-400 text-[18px] p-2">The page you are looking for is not available</p>
-                <button className="p-1 bg-linear-to-br from-purple-600 to bg-purple-500 rounded-md text-[13px] font-bold btn border-none h-7.5 w-20" onClick={() => handlePageBack()}>Go Back!</button>
+                <button className="p-1 bg-linear-to-br from-purple-600 to bg-purple-500 rounded-md text-[13px] font-bold btn border-none h-7.5 w-20 cursor-pointer" onClick={() => navigate(- 1)}>Go Back!</button>
             </div>
         </div>
     );
